7968165110020d2f00d867f7695a9623533b1a9e
kaiwu119 <kaiwu119@163.com>
2021-10-28 14:25:18 +0800

    Add premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -14,7 +14,7 @@ project "ImGui"
         "imgui_draw.cpp",
         "imgui_internal.h",
         "imgui_widgets.cpp",
-        "imgui_tables.cpp"
+        "imgui_tables.cpp",
         "imstb_rectpack.h",
         "imstb_textedit.h",
         "imstb_truetype.h"
