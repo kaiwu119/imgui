75a2f5678339b97703be39f4ecfa550ed1f9ad24
kaiwu119 <kaiwu119@163.com>
2021-10-31 15:27:47 +0800

    add premake file

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"  
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imgui_tables.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h"
+    }
+    defines
+        {
+            "IMGUI_API=__declspec( dllexport )"
+        }
+	filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+
+    filter "configurations:Debug"
+	runtime "Debug"
+	symbols "on"
+
+    --filter { "system:windows", "configurations:Release" }
+     --   buildoptions "/MT"
